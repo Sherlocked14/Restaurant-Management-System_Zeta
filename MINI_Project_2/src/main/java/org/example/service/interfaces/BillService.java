2c10aca1714137943e363489414b33e79c700127
Kushagra Shukla <kushagrash@zeta.tech>
2025-08-14 11:03:32 +0530

    remove unused service and DAO interfaces

--- MINI_Project_2/src/main/java/org/example/service/interfaces/BillService.java
+++
@@ -1,9 +0,0 @@
-package org.example.service.interfaces;
-
-import org.example.model.Bill;
-
-public interface BillService {
-    void generateBill(Bill bill);
-    Bill getBillByOrderId(int orderId);
-    void updatePaymentStatus(int billId, String paymentStatus);
-}
--- MINI_Project_2/src/main/java/org/example/service/interfaces/PaymentService.java
+++
@@ -1,7 +0,0 @@
-package org.example.service.interfaces;
-import org.example.model.Payment;
-
-public interface PaymentService {
-    void processPayment(Payment payment);
-    Payment getPaymentByBillId(int billId);
-}
